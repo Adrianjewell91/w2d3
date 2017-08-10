80fd92361b671ed1e824d3100071c8d9ea1b98b2
App Academy Student <student@appacademy.io>
2017-08-09 17:42:41 -0700

    almost done with deck'

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -2,19 +2,36 @@ VALUE = %w[A K Q J 10 9 8 7 6 5 4 3 2]
 SUITE = %w[S H D C]
 
 class Deck
-  attr_reader :unused_cards,:used_cards
+  attr_reader :unused_cards, :discard_pile
 
-  def initialize
-    @unused_cards = []
-    @used_cards = []
-  end
-
-  def populate
+  def self.populate
+    cards = []
     4.times do |i|  # .times implicitly returns # of times
       VALUE.each do |value|
-        @unused_cards << "#{value}-#{SUITE[i]}"
+        cards << "#{value}-#{SUITE[i]}"
       end
     end
-    @unused_cards # needed
+    Deck.new(cards) # needed
+  end
+
+  def initialize(cards) #Deck.new only calls initialize
+    @unused_cards = cards
+    @discard_pile = []
+  end
+
+  def shuffle_deck
+    @unused_cards.shuffle
+  end
+
+  def count
+    @unused_cards.count
+  end
+
+  def take
+    @unused_cards.shift
+  end
+
+  def discard(card)
+    @discard_pile << card
   end
 end
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,27 +1,54 @@
 require 'deck'
 require 'rspec'
 
-
 describe Deck do
-  subject(:deck) { Deck.new }
+  subject(:deck) { Deck.populate } # memoization => not creating a new deck
   describe '#initialize' do
-    it "should create an empty arr of unused cards" do
-      expect(deck.unused_cards).to eq([])
+    it "should have a deck of cards" do
+      expect(deck.unused_cards).to be_a(Array)
     end
 
     it "should create an empty arr of used cards" do
-      expect(deck.used_cards).to eq([])
+      expect(deck.discard_pile).to eq([])
     end
   end
 
   describe "populate" do
     it "should populate the deck with 52 cards" do
-      expect(deck.populate.length).to eq(52)
+      expect(deck.unused_cards.length).to eq(52)
     end
 
     it "should have thirteen hearts" do
-      hearts = deck.populate.count { |el| el.include?('H') }
+      hearts = deck.unused_cards.count { |el| el.include?('H') }
       expect(hearts).to eq(13)
     end # could test for other 3 suites
+  end
+
+  describe "#shuffle_deck" do
+    it "should shuffle the populated deck" do
+      expect(deck.unused_cards).not_to eq(deck.shuffle_deck)
+    end
+  end
+
+  describe '#count' do
+    it "should return the # of cards left in deck" do
+      expect(deck.count).to eq(52)
+    end
+  end
+
+  describe '#take' do
+    it "should take a card from the deck" do
+      deck.take
+      expect(deck.count).to eq(51)
+    end
+  end
+
+  describe '#discard' do # create a dummy card that deck adds discards
+    let (:card) { double("card") }
+
+    it "should add a card to the discard pile" do
+      deck.discard(card)
+      expect(deck.discard_pile).to include(card)
+    end
   end
 end
